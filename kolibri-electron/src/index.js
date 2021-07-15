e217c7c0df9cc6594534d6f1b23afb1845fb079a
danigm <dani@danigm.net>
2021-07-15 03:47:08 -0700

    electron: First launch experience
    This patch adds some verification for the application first launch.
    
    1. Check for the file in ~/.endless-key/version.
    2. If the file doesn't exists, we launch the first launch experience.
    3. If the file exists it compares the version with the
       kolibri-explore-plugin version in the key.
    4. If the version is different, the directory ~/.endless-key is removed
       (an replaced with the one in the key) and the cache is cleaned.
    5. If the version is the same, we show the normal loading screen.
    
    This patch also tries to load the loadingScreen from the Endless Key,
    looking into the following directory:
    D:/KOLIBRI_DATA/extensions/kolibri_explore_plugin/loadingScreen/index.html
    
    This way we can implement the loading screen as a vue app inside the
    kolibri-explore-plugin repository and will be updated with every new
    plugin release.
    
    By default, the loading screen fallback to the default kolibri loading
    screen, so this change continue working without the loadingScreen app in
    the key yet.
    
    https://phabricator.endlessm.com/T32227

--- kolibri-electron/src/index.js
+++ kolibri-electron/src/index.js
@@ -2,6 +2,10 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const child_process = require('child_process');
 const http = require('http');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const os = require('os');
+const drivelist = require('drivelist');
 const { shell } = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -9,15 +13,126 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
+const NULL_PLUGIN_VERSION = '0';
 const KOLIBRI = 'http://localhost:5000';
-const pingTimeout = 20;
+let pingTimeout = 20;
+let mainWindow = null;
 let loadRetries = 0;
 let timeSpent = 0;
 let maxRetries = 3;
 
 let django = null;
 
-const waitForKolibriUp = (mainWindow) => {
+async function getEndlessKeyDrive() {
+  const drives = await drivelist.list();
+
+  // Look for the endless key drive
+  for (const d of drives.map((d) => d.mountpoints[0].path)) {
+    console.log(`Looking for endless Key on ${d}`);
+    const p = path.join(d, 'KOLIBRI_DATA');
+    try {
+      await fsPromises.access(p);
+      return d;
+    } catch (err) {
+      console.log(`${p} doesn't exists, trying next`);
+    }
+  };
+
+  return null;
+}
+
+async function getLoadingScreen() {
+  const defaultLoading = path.join(__dirname, 'Kolibri', 'assets', '_load.html');
+
+  // Try to use the custom loading screen from kolibri-explore-plugin
+  const drive = await getEndlessKeyDrive();
+  if (!drive) {
+    return defaultLoading;
+  }
+
+  const loading = path.join(
+    drive,
+    'KOLIBRI_DATA',
+    'extensions',
+    'kolibri_explore_plugin',
+    'loadingScreen',
+    'index.html',
+  );
+
+  try {
+    await fsPromises.access(loading);
+    return loading;
+  } catch (err) {
+    console.log(`Loading screen not found ${loading}`);
+  }
+
+  return defaultLoading;
+}
+
+async function getPluginVersion() {
+  const drive = await getEndlessKeyDrive();
+  if (!drive) {
+    console.error('Endless Key not found');
+    return NULL_PLUGIN_VERSION;
+  }
+  const extensionsDir = path.join(drive, 'KOLIBRI_DATA', 'extensions');
+
+  try {
+    const files = await fsPromises.readdir(extensionsDir);
+    for (const file of files) {
+      // Looking for plugin kolibri_explore_plugin-VERSION.dist-info
+      const re = /kolibri_explore_plugin-(\d+\.\d+\.\d+)\.dist-info/
+      const match = file.match(re);
+      if (match) {
+        return match[1];
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  return NULL_PLUGIN_VERSION;
+}
+
+// Checks for the ~/.endless-key/version file and compares with the Endless key
+// kolibri-explore-plugin version. If the version is different, the
+// ~/endless-key folder will be removed.
+//
+// Return true if the version file does not match the plugin version or if the
+// .endless-key doesn't exists.
+async function checkVersion() {
+  console.log('Checking kolibri-app version file');
+  const kolibriHome = path.join(os.homedir(), '.endless-key');
+  const versionFile = path.join(kolibriHome, 'version');
+  const pluginVersion = await getPluginVersion();
+  let kolibriHomeVersion = NULL_PLUGIN_VERSION;
+
+  try {
+    kolibriHomeVersion = fs.readFileSync(versionFile, 'utf8').trim();
+  } catch (error) {
+    console.log('No version file found in .endless-key');
+  }
+
+  console.log(`${kolibriHomeVersion} < ${pluginVersion}`);
+  if (kolibriHomeVersion < pluginVersion) {
+    console.log('Newer version, replace the .endless-key directory and cleaning cache');
+    await fsPromises.rm(kolibriHome, { recursive: true, force: true });
+    mainWindow.webContents.session.clearCache();
+    return true;
+  }
+
+  return false;
+}
+
+async function updateVersion() {
+  const kolibriHome = path.join(os.homedir(), '.endless-key');
+  const versionFile = path.join(kolibriHome, 'version');
+  const pluginVersion = await getPluginVersion();
+
+  await fsPromises.writeFile(versionFile, pluginVersion);
+}
+
+const waitForKolibriUp = () => {
   console.log('Kolibri server not yet started, checking again in one second...');
 
   if (timeSpent > pingTimeout) {
@@ -32,7 +147,6 @@ const waitForKolibriUp = (mainWindow) => {
       waitForKolibriUp(mainWindow);
     } else {
       contents.executeJavaScript('show_error()', true);
-      mainWindow.loadFile(path.join(__dirname, 'Kolibri', 'assets', '_load.html'));
     }
 
     return;
@@ -40,21 +154,24 @@ const waitForKolibriUp = (mainWindow) => {
 
   http.get(`${KOLIBRI}/api/public/info`, (response) => {
     mainWindow.loadURL(KOLIBRI);
+    updateVersion();
   }).on("error", (error) => {
     console.log("Error: " + error.message);
     setTimeout(() => { waitForKolibriUp(mainWindow); timeSpent++; }, 1000);
   });
 };
 
-const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+async function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
     autoHideMenuBar: true,
     center: true,
     icon: path.join(__dirname, 'icon.png'),
+    show:false,
   });
   mainWindow.maximize();
+  mainWindow.show();
 
   // Link handler to open external links on default browser
   const windowOpenHandler = ({url}) => {
@@ -67,9 +184,14 @@ const createWindow = () => {
     return {action: 'deny'};
   };
   mainWindow.webContents.setWindowOpenHandler(windowOpenHandler);
+  await mainWindow.loadFile(await getLoadingScreen());
 
-  // Load the loading screen
-  mainWindow.loadFile(path.join(__dirname, 'Kolibri', 'assets', '_load.html'));
+  const firstLaunch = await checkVersion();
+  if (firstLaunch) {
+    mainWindow.webContents.executeJavaScript('firstLaunch()', true);
+    // Adding 20 seconds to the pingTimeout to wait for the kolibri-home copy
+    pingTimeout += 20;
+  }
   waitForKolibriUp(mainWindow);
 };
 
@@ -95,8 +217,10 @@ const runKolibri = () => {
 };
 
 app.on('ready', () => {
-  createWindow();
-  runKolibri();
+  createWindow()
+    .then(() => {
+      runKolibri();
+    });
 });
 
 app.on('window-all-closed', () => {
