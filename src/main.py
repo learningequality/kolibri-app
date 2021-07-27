d738d567772e20e6da5247d5eaa7070102acf288
Dylan McCall <dylan@endlessos.org>
2021-07-27 12:05:58 -0700

    Add kolibri_zim_plugin to includes
    https://phabricator.endlessm.com/T32061

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
+import builtins
 import datetime
-import gettext
 import logging
 import os
-import pew
 import sys
+
+from functools import partial
 
 from config import KOLIBRI_PORT
 from kolibri_tools.utils import start_kolibri_server
@@ -61,33 +62,8 @@ locale_root_dir = os.path.join(root_dir, 'locale')
 sys.path.insert(0, extra_python_path)
 sys.path.insert(0, os.path.join(extra_python_path, "kolibri", "dist"))
 
-pew.set_app_name("Kolibri")
-
 KOLIBRI_ROOT_URL = 'http://localhost:{}'.format(KOLIBRI_PORT)
 os.environ["DJANGO_SETTINGS_MODULE"] = "kolibri.deployment.default.settings.base"
-
-app_data_dir = pew.get_app_files_dir()
-os.makedirs(app_data_dir, exist_ok=True)
-
-languages = None
-locale_info = {}
-try:
-    t = gettext.translation('macapp', locale_root_dir, languages=languages, fallback=False)
-    locale_info = t.info()
-    # We have not been able to reproduce, but we have seen this happen in user tracebacks, so
-    # trigger the exception handling fallback if locale_info doesn't have a language key.
-    if not 'language' in locale_info:
-        raise Exception("Received invalid language_info dict.")
-    _ = t.gettext
-
-except Exception as e:
-    # Fallback to English and if we fail to find any language catalogs.
-    locale_info['language'] = 'en_US'
-    def _(text):
-        return text
-    logging.warning("Error retrieving language: {}".format(repr(e)))
-
-logging.info("Locale info = {}".format(locale_info))
 
 
 class Application:
